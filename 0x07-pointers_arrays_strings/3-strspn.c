64e2d7f83f9b01e5158a5af2fd63adccdb620e5e
Lukerosa <truzariro@gmail.com>
2023-04-04 18:01:08 -0700

    Strings Arrays

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,23 +8,23 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int len = 0;
-	int i, j;
-	
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		for (j = 0; accept[j] != '\0'; j++)
-		{
-			if (s[i] == accept[j])
-			{
-				len++;
-				break;
-			}
-		}
-		if (accept[j] == '\0')
-		{
-			return (len); // return length of prefix substring once end of accept string is reached
-		}
-	}
-	return (len); // return length of prefix substring
+    unsigned int len = 0;
+    int i, j;
+    
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        for (j = 0; accept[j] != '\0'; j++)
+        {
+            if (s[i] == accept[j])
+            {
+                len++;
+                break;
+            }
+        }
+        if (accept[j] == '\0')
+        {
+            return (len); /* return length of prefix substring once end of accept string is reached */
+        }
+    }
+    return (len); /* return length of prefix substring */
 }

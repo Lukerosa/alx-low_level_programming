7b000281fef9f378f7ab1fb73e7c6ca92d634c1e
Lukerosa <36511433+Lukerosa@users.noreply.github.com>
2023-04-13 01:47:19 +0200

    Malloc_Free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@
 
 char *create_array(unsigned int size, char c)
 {
-    unsigned int i;
+	unsigned int i;
 	char *length;
 
 	length = malloc(size * sizeof(char));
@@ -24,17 +24,5 @@ char *create_array(unsigned int size, char c)
 	{
 		length[i] = c;
 	}
-	return (length);char* create_array(unsigned int size, char c) {
-    if (size == 0) {
-        return NULL;
-    }
-    char* arr = (char*) malloc(size * sizeof(char));
-    if (arr == NULL) {
-        return NULL;
-    }
-    for (unsigned int i = 0; i < size; ++i) {
-        arr[i] = c;
-    }
-    return arr;
-    }
+	return (length);
 }

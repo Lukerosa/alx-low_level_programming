a8f37f852937e1323d04906793bad32e0e861c23
Lukerosa <36511433+Lukerosa@users.noreply.github.com>
2023-04-13 22:18:50 +0200

    More Mallocs

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - Entry point
+ * Description: Allocates memory using malloc
+ * @b: Integer
+ * Return: ptr
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+    if (ptr == NULL) {
+        fprintf(stderr, "malloc failed\n");
+        exit(98);
+    }
+    return ptr;
+}

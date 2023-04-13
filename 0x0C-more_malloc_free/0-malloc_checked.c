51818948122bc6dc2c1c6cb130309faa1738b04d
Lukerosa <36511433+Lukerosa@users.noreply.github.com>
2023-04-13 22:38:21 +0200

    More Malloc Free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -12,9 +12,10 @@
 void *malloc_checked(unsigned int b)
 {
 	void *ptr = malloc(b);
-    if (ptr == NULL) {
-        fprintf(stderr, "malloc failed\n");
-        exit(98);
-    }
-    return ptr;
-}
+	if (ptr == NULL)
+	{
+		fprintf(stderr, "malloc failed\n");
+		exit(98);
+		}
+		return ptr;
+		}

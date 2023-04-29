18566b216efb8ecc1868e8966bb945503563afb9
Lukerosa <truzariro@gmail.com>
2023-04-28 17:44:52 -0700

    Singly Linked Lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,6 +2,10 @@
 
 void __attribute__((constructor)) flash();
 
+/**
+ * flash - Prints strings before the main function is executed
+ */
+
 void flash(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
